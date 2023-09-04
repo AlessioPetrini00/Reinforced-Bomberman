8463b5f1abafddb84ece14b15a5711bddabebc6b
Alessio Petrini <alessio.petrini@mail.polimi.it>
2023-09-04 19:46:26 +0200

    Replaced callbacks.py and train.py files in my_agent with those provided in tpl_agent

--- agent_code/my_agent/callbacks.py
+++ agent_code/my_agent/callbacks.py
@@ -1,33 +1,79 @@
-# This file is always loaded at the start of a new game.
+import os
+import pickle
+import random
 
-'''
-Called once before the start of a new round.
-Use this to initialize everthing you'll need during the game.
-The self argument is a persistent object, you can assign values to it which you might need later on.
-'''
+import numpy as np
+
+
+ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
+
+
 def setup(self):
-    return
-    
-'''
-Called once every step to give your agent the opportunity to pick the best move.
-The possible return values are:
-• UP
-• DOWN
-• LEFT
-• RIGHT
-• BOMB
-• WAIT
-game_state is a dictionary that defines the current state of the world. 
-It contains:
-• round (int): Number of rounds since launching of the environment. Starts at 1.
-• step (int): Number of steps since beginning of round. Starts at 1.
-• field (np.array(width, height)): Describes the tiles of the game world: 1 for crates, -1 for walls and 0 for free tiles.
-• bombs ([(int, int), int]): a list of tuples ((x, y), t) for all placed bombs and their countdowns (countdown == 0 means that the bomb is about to explode).
-• explosion_map (np.array(width, height)): stating for each tile for how many more steps the explosion will be present (0 where there’s no explosion).
-• coins ([(x,y)]): list of coordinates for all currently collectable coins.
-• self ((str, int, bool, (int, int))): tuple (n, s, b, (x, y) describing your agent: n is the name, s is the score, b indicates wether it is possible to drop a bomb or not and (x, y) is the position.
-• others ([(str, int, bool, (int, int))]): list of tuples similar to the one described above to keep track of the opponents.
-• user_input (str/None): user input via GUI.
-'''
-def act(self, game_state):
-    return
+    """
+    Setup your code. This is called once when loading each agent.
+    Make sure that you prepare everything such that act(...) can be called.
+
+    When in training mode, the separate `setup_training` in train.py is called
+    after this method. This separation allows you to share your trained agent
+    with other students, without revealing your training code.
+
+    In this example, our model is a set of probabilities over actions
+    that are is independent of the game state.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    """
+    if self.train or not os.path.isfile("my-saved-model.pt"):
+        self.logger.info("Setting up model from scratch.")
+        weights = np.random.rand(len(ACTIONS))
+        self.model = weights / weights.sum()
+    else:
+        self.logger.info("Loading model from saved state.")
+        with open("my-saved-model.pt", "rb") as file:
+            self.model = pickle.load(file)
+
+
+def act(self, game_state: dict) -> str:
+    """
+    Your agent should parse the input, think, and take a decision.
+    When not in training mode, the maximum execution time for this method is 0.5s.
+
+    :param self: The same object that is passed to all of your callbacks.
+    :param game_state: The dictionary that describes everything on the board.
+    :return: The action to take as a string.
+    """
+    # todo Exploration vs exploitation
+    random_prob = .1
+    if self.train and random.random() < random_prob:
+        self.logger.debug("Choosing action purely at random.")
+        # 80%: walk in any direction. 10% wait. 10% bomb.
+        return np.random.choice(ACTIONS, p=[.2, .2, .2, .2, .1, .1])
+
+    self.logger.debug("Querying model for action.")
+    return np.random.choice(ACTIONS, p=self.model)
+
+
+def state_to_features(game_state: dict) -> np.array:
+    """
+    *This is not a required function, but an idea to structure your code.*
+
+    Converts the game state to the input of your model, i.e.
+    a feature vector.
+
+    You can find out about the state of the game environment via game_state,
+    which is a dictionary. Consult 'get_state_for_agent' in environment.py to see
+    what it contains.
+
+    :param game_state:  A dictionary describing the current game board.
+    :return: np.array
+    """
+    # This is the dict before the game begins and after it ends
+    if game_state is None:
+        return None
+
+    # For example, you could construct several channels of equal shape, ...
+    channels = []
+    channels.append(...)
+    # concatenate them as a feature tensor (they must have the same shape), ...
+    stacked_channels = np.stack(channels)
+    # and return them as a vector
+    return stacked_channels.reshape(-1)
